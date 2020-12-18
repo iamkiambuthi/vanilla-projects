647425463a1fdf46d0f92709423b88a6dc2c2cfa
iamkiambuthi <iamkiambuthi@gmail.com>
2020-12-19 01:40:45 +0300

    added 05

---
+++ projects/05_Countdown/assets/scripts/scripts.js
@@ -0,0 +1,7 @@
+const yearItem = document.querySelector("year-item");
+const monthItem = document.querySelector("month-item");
+const hourItem = document.querySelector("hour-item");
+const minuteItem = document.querySelector("minute-item");
+const secondItem = document.querySelector("second-item");
+
+const launchDate = new Date(2021, 5, 5);

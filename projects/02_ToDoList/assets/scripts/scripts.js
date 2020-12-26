f11e0a7e422e9bdb4d71921d99f72a6bee5e3030
iamkiambuthi <iamkiambuthi@gmail.com>
2020-12-26 16:16:19 +0300

    finished 02_ToDoList

--- projects/02_ToDoList/assets/scripts/scripts.js
+++ projects/02_ToDoList/assets/scripts/scripts.js
@@ -1,200 +1,57 @@
-/**
-* scripts.js
-* iamkiambuthi. 2020.
-* MIT LICENSE
-*/
-
-/*
-* TODO
-* =====
-* 1. Delete Btn not working.
-* 2. Push the last item above the bottom div.
-*
-*/
 window.onload = function() {
-	const body = document.querySelector("#root");
-	var taskList = [];
+	const todoForm = document.querySelector(".todo-form");
+	const taskElement = document.querySelector(".todo-tasks");
+	let taskList = [];
 
-	var getRank = function() {
-		var rank = taskList.length;
-		rank += 1;
+	function createTaskItem(taskLabel) {
+		/* Create an li element with a delete btn */
+		const element = document.createElement("li");
+		element.className = "task-item"
+		const taskLabelItem = document.createElement("div");
+		taskLabelItem.className = "task-label"
+		const taskDeleteItem = document.createElement("button");
+		taskDeleteItem.innerHTML = "remove";
+		taskDeleteItem.className = "task-delete";
 
-		return rank;
+		element.appendChild(taskLabelItem);
+		element.appendChild(taskDeleteItem);
+
+		taskLabelItem.innerHTML = taskLabel;
+		taskElement.appendChild(element);
 	}
 
-	var getGenesis = function() {
-		var DateTime = new Date().toDateString();
+	function updateDOM() {
+		/* Add an event listener to all delete buttons. */
+		const deleteBtns = document.querySelectorAll(".task-delete");
 
-		return DateTime;
-	}
-
-	function createTask(tLabel) {
-		// task object.
-
-		var task = new Object();
-		task.label = tLabel;
-		task.rank = getRank();
-		task.dateCreated = getGenesis();
-
-		// add to the list.
-		taskList.push(task);
-		return task;
-	}
-
-	var createTaskInputItem = function() {
-		/**
-		* Creates a DOM element to handle task input.
-		* 
-		* layout
-		* =======
-		* task-input-item
-		*
-		* <tii-wrapper>
-		* 	<tii-section>
-		* 		<tii-header></tti-header>
-		*
-		* 		<tii-body>
-		*			<tii-input></tii-input>
-		* 			<tii-button></tii-button>
-		* 		</tii-body>
-		*
-		* 		<tii-footer></tii-footer>
-		* 	</tii-section>
-		* </tii-wrapper>
-		*/
-
-		var wrapper = document.createElement("div");
-		wrapper.className = "tii-w";
-
-		var taskInputItem = document.createElement("div");
-		taskInputItem.className = "tii-s";
-
-		var taskInputItemHeader = document.createElement("div");
-		taskInputItemHeader.className = "tii-h";
-		taskInputItemHeader.innerHTML = "Add a Task";
-
-		var taskInputItemBody = document.createElement("div");
-		taskInputItemBody.className = "tii-b";
-
-		var taskInput = document.createElement("input");
-		taskInput.className = "tii-input";
-		taskInput.placeholder = "Enter task name";
-
-		var taskCreate = document.createElement("button");
-		taskCreate.className = "tii-button";
-		taskCreate.innerHTML = "create";
-
-		var taskInputItemFooter = document.createElement("div");
-		taskInputItemFooter.className = "tii-f";
-
-		taskInputItemBody.appendChild(taskInput);
-		taskInputItemBody.appendChild(taskCreate);
-
-		taskInputItem.appendChild(taskInputItemHeader);
-		taskInputItem.appendChild(taskInputItemBody);
-		taskInputItem.appendChild(taskInputItemFooter);
-
-		wrapper.appendChild(taskInputItem);
-
-		return wrapper
-	}
-
-	var updateDisplay = function(arr, element) {
-		/**
-		* Creates a DOM element to display output.
-		* 
-		* task-display-wrapper
-		*
-		* FIX: instead of creating the entire element each and every time.
-		* only create whats needed.
-		*
-		* layout
-		* =======
-		* <td-w>
-		* 	<td-s>
-		*		<td>
-		* 			<tr-w>
-		*				<tr></tr>
-		* 			</tr-w>
-		*
-		* 			<tl-w>
-		* 				<tl></tl>
-		* 				<tdi></tdi>
-		*			</tl-w>
-		* 			
-		* 			<td-w>
-		*				<td></td>
-		* 			</td-w>
-		* 		</td>
-		*   </td-s>
-		* </td-w>
-		*/
-		var wrapper = document.createElement("div");
-		wrapper.className = "td-w";
-
-		var taskDisplaySection = document.createElement("div");
-		taskDisplaySection.className = "td-s";
-
-		var taskDisplay = document.createElement("div");
-		taskDisplay.className = "td";
-
-		var taskRankWrapper = document.createElement("div");
-		taskRankWrapper.className = "tr-w";
-
-		var taskRank = document.createElement("div");
-		taskRank.className = "tr";
-
-		var taskLabelWrapper = document.createElement("div");
-		taskLabelWrapper.className = "tl-w";
-
-		var taskLabel = document.createElement("div");
-		taskLabel.className = "tl";
-
-		var taskDateItem = document.createElement("div");
-		taskDateItem.className = "tdi";
-
-		var taskDeleteWrapper = document.createElement("div");
-		taskDeleteWrapper.className = "td-w";
-
-		var taskDelete = document.createElement("button");
-		taskDelete.className = "td-btn";
-		taskDelete.id = "delBtn";
-		taskDelete.innerHTML = "delete";
-
-		taskRankWrapper.appendChild(taskRank);
-		taskLabelWrapper.appendChild(taskLabel);
-		taskLabelWrapper.appendChild(taskDateItem);
-		taskDeleteWrapper.appendChild(taskDelete);
-
-		taskDisplay.appendChild(taskRankWrapper);
-		taskDisplay.appendChild(taskLabelWrapper);
-		taskDisplay.appendChild(taskDeleteWrapper);
-
-		taskDisplaySection.appendChild(taskDisplay);
-
-
-		for (var i=0; i < arr.length; i++) {
-			// insert the values.
-			taskRank.innerHTML = arr[i].rank;
-			taskLabel.innerHTML = arr[i].label;
-			taskDateItem.innerHTML = arr[i].dateCreated;
+		for (let i = 0; i < deleteBtns.length; i++) {
+			deleteBtns[i].onclick = function() {
+				// Remove task item from the list.
+				taskItem = this.parentElement;
+				taskItem.parentNode.removeChild(taskItem);
+			}
 		}
-		element.appendChild(taskDisplaySection);
 	}
 
-	var item = createTaskInputItem();
-	body.appendChild(item);
+
+	todoForm.addEventListener("submit", function(e) {
+		// Prevent form from being sumitted.
+		e.preventDefault();
+		// Retreive the value of the input field.
+		const taskName =this.taskLabel.value;
+		if (taskName != ""){
+			createTaskItem(taskName);
+			updateDOM();
+		} else {
+			const errDisplayField = document.querySelector(".error-field");
+			errDisplayField.innerHTML = "Enter a task name to create one";
+
+		}
+		
+		
+	});
 
 
-	// adding an item.
-	var btn = item.querySelector(".tii-button");
-
-	btn.onclick = function() {
-		t = item.querySelector(".tii-input").value;
-		task = createTask(t);
-		updateDisplay(taskList, body);
-
-	}
 }
 
 
